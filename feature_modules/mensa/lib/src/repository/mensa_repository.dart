39ef295c9e316e21ecd8cc1444f6c965e4016a4e
BitterSchoki <65418896+BitterSchoki@users.noreply.github.com>
2024-05-14 17:39:30 +0200

    Implement Caching for Mensa Models (#2)
    * Implement caching for mensa models
    
    * Add shared preferences dependency
    
    * Display mensa names on overview page
    
    ---------
    
    Co-authored-by: Paul <paul.walter@partner.bmw.de>

--- feature_modules/mensa/lib/src/repository/mensa_repository.dart
+++ feature_modules/mensa/lib/src/repository/mensa_repository.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'api/mensa_api_client.dart';
 import 'api/models/mensa_model.dart';
 
 abstract class MensaRepository {
-  Future<List<MensaModel>> getMensaOverview();
+  Future<List<MensaModel>> getMensaModels();
 }
 
 class ConnectedMensaRepository implements MensaRepository {
@@ -12,8 +16,38 @@ class ConnectedMensaRepository implements MensaRepository {
 
   final MensaApiClient mensaApiClient;
 
+  static const String _mensaModelsCacheKey = 'mena_models_cache_key';
+  static const String _mensaModelCacheDateKey = 'mensa_models_cache_date_key';
+  static const Duration _mensaModelsCacheDuration = Duration(days: 30);
+
   @override
-  Future<List<MensaModel>> getMensaOverview() {
-    return mensaApiClient.getMensaOverview();
+  Future<List<MensaModel>> getMensaModels({bool forceRefresh = false}) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final cachedData = prefs.getString(_mensaModelsCacheKey);
+    final cachedTime = prefs.getString(_mensaModelCacheDateKey);
+    final cachedDateTime = cachedTime != null ? DateTime.parse(cachedTime) : null;
+    final cachedTimeExceeded =
+        cachedDateTime != null && DateTime.now().difference(cachedDateTime) > _mensaModelsCacheDuration;
+
+    if (cachedData != null && !cachedTimeExceeded && !forceRefresh) {
+      final jsonList = json.decode(cachedData) as List<dynamic>;
+      return jsonList.map((json) => MensaModel.fromJson(json as Map<String, dynamic>)).toList();
+    }
+
+    try {
+      final mensaModels = await mensaApiClient.getMensaModels();
+      final jsonResponse = json.encode(mensaModels.map((mensa) => mensa.toJson()).toList());
+      await prefs.setString(_mensaModelsCacheKey, jsonResponse);
+      await prefs.setString(_mensaModelCacheDateKey, DateTime.now().toIso8601String());
+
+      return mensaModels;
+    } catch (e) {
+      if (cachedData != null) {
+        final jsonList = json.decode(cachedData) as List<dynamic>;
+        return jsonList.map((json) => MensaModel.fromJson(json as Map<String, dynamic>)).toList();
+      }
+      rethrow;
+    }
   }
 }
