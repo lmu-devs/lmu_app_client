8917f182a9a3f98df152cd8d898f48d8565cbbb3
Paul <paul.walter@partner.bmw.de>
2024-10-24 14:46:09 +0200

    (feature): add LmuTileHeadline component

---
+++ core/lib/src/components/tiles/lmu_tile_headline.dart
@@ -0,0 +1,85 @@
+import 'package:core/components.dart';
+import 'package:core/constants.dart';
+import 'package:core/themes.dart';
+import 'package:flutter/material.dart';
+
+class LmuTileHeadline extends StatelessWidget {
+  final String title;
+  final String? actionTitle;
+  final void Function()? onActionTap;
+  final Widget? bottomWidget;
+
+  const LmuTileHeadline._internal({
+    super.key,
+    required this.title,
+    this.actionTitle,
+    this.onActionTap,
+    this.bottomWidget,
+  });
+
+  factory LmuTileHeadline.base({
+    Key? key,
+    required String title,
+    Widget? bottomWidget,
+  }) =>
+      LmuTileHeadline._internal(
+        key: key,
+        title: title,
+        bottomWidget: bottomWidget,
+      );
+
+  factory LmuTileHeadline.action({
+    Key? key,
+    required String title,
+    required String actionTitle,
+    required void Function() onActionTap,
+    Widget? bottomWidget,
+  }) =>
+      LmuTileHeadline._internal(
+        key: key,
+        title: title,
+        actionTitle: actionTitle,
+        onActionTap: onActionTap,
+        bottomWidget: bottomWidget,
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            LmuText.body(
+              title,
+              weight: FontWeight.w600,
+              color: context.colors.neutralColors.textColors.mediumColors.base,
+            ),
+            if (actionTitle != null)
+              GestureDetector(
+                onTap: onActionTap,
+                child: LmuText.body(
+                  actionTitle,
+                  color: context.colors.brandColors.textColors.strongColors.base,
+                  weight: FontWeight.w600,
+                ),
+              ),
+          ],
+        ),
+        const SizedBox(height: LmuSizes.mediumLarge),
+        if (bottomWidget != null)
+          Column(
+            children: [
+              ConstrainedBox(
+                constraints: const BoxConstraints(
+                  maxHeight: 36,
+                ),
+                child: bottomWidget!,
+              ),
+              const SizedBox(height: LmuSizes.mediumLarge),
+            ],
+          ),
+      ],
+    );
+  }
+}
--- core/lib/src/components/tiles/tiles.dart
+++ core/lib/src/components/tiles/tiles.dart
@@ -1,2 +1,3 @@
 export 'base_tile.dart';
 export 'lmu_content_tile.dart';
+export 'lmu_tile_headline.dart';
