14bca1a8340d1484cf649947264e4611b0480605
Paul <paul.walter@partner.bmw.de>
2024-11-07 17:17:19 +0100

    (chore): add mensa model to details route

--- feature_modules/mensa/lib/src/pages/mensa_details_page.dart
+++ feature_modules/mensa/lib/src/pages/mensa_details_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
-import 'package:mensa/src/bloc/bloc.dart';
 import 'package:mensa/src/bloc/mensa_menu_cubit/mensa_menu_cubit.dart';
 import 'package:mensa/src/repository/api/api.dart';
 import 'package:mensa/src/repository/mensa_repository.dart';
@@ -11,11 +9,13 @@ import '../views/mensa_details_view.dart';
 class MensaDetailsPage extends StatelessWidget {
   const MensaDetailsPage({
     super.key,
+    required this.mensaModel,
   });
+
+  final MensaModel mensaModel;
 
   @override
   Widget build(BuildContext context) {
-    final mensaModel = (GetIt.I.get<MensaCubit>().state as MensaLoadSuccess).mensaModels.first;
     final mensaRepository = ConnectedMensaRepository(
       mensaApiClient: MensaApiClient(),
     );
