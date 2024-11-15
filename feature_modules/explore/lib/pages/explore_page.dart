4a18b5ee04d27d55986fb7fffd4e66d74bab434f
Lukas Plenk <lukasplenk@yahoo.de>
2024-11-15 13:54:11 +0100

    feature: added markers, env file and some fixes for build

--- feature_modules/explore/lib/pages/explore_page.dart
+++ feature_modules/explore/lib/pages/explore_page.dart
@@ -1,9 +1,13 @@
+import 'package:core/constants.dart';
 import 'package:flutter/material.dart' hide Visibility;
 import 'package:get_it/get_it.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'package:mensa/mensa.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:core/themes.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ExplorePage extends StatelessWidget {
   const ExplorePage({super.key});
@@ -14,15 +18,15 @@ class ExplorePage extends StatelessWidget {
   }
 }
 
-class AnnotationClickListener extends OnCircleAnnotationClickListener {
+class AnnotationClickListener extends OnPointAnnotationClickListener {
   AnnotationClickListener({
     required this.onAnnotationClick,
   });
 
-  final void Function(CircleAnnotation annotation) onAnnotationClick;
+  final void Function(PointAnnotation annotation) onAnnotationClick;
 
   @override
-  void onCircleAnnotationClick(CircleAnnotation annotation) {
+  void onPointAnnotationClick(PointAnnotation annotation) {
     print("onAnnotationClick, id: ${annotation.id}");
     onAnnotationClick(annotation);
   }
@@ -39,51 +43,112 @@ class MapWithAnnotationsState extends State<MapWithAnnotations> {
   MapWithAnnotationsState();
 
   MapboxMap? mapboxMap;
-  CircleAnnotation? circleAnnotation;
-  CircleAnnotationManager? circleAnnotationManager;
-  int styleIndex = 1;
+  PointAnnotationManager? pointAnnotationManager;
   List<MensaLocationData> mensaLocations = [];
 
-  void _onMapCreated(MapboxMap mapboxMap) {
-    this.mapboxMap = mapboxMap;
-    mapboxMap.annotations.createCircleAnnotationManager().then((value) {
-      circleAnnotationManager = value;
-      var options = <CircleAnnotationOptions>[];
-      for (final location in mensaLocations) {
-        options.add(
-          CircleAnnotationOptions(
-            geometry: Point(
-              coordinates: Position(
-                location.longitude,
-                location.latitude,
-              ),
-            ),
-            circleRadius: 12.0,
-          ),
-        );
-      }
+  Future<void> _requestLocationPermission() async {
+    final prefs = await SharedPreferences.getInstance();
+    bool isFirstLaunch = prefs.getBool('isFirstLaunch') ?? true;
 
-      circleAnnotationManager?.createMulti(options);
-      circleAnnotationManager?.addOnCircleAnnotationClickListener(
-        AnnotationClickListener(
-          onAnnotationClick: (annotation) {
-            mapboxMap.easeTo(
-              CameraOptions(
-                center: annotation.geometry,
-                zoom: 14.0,
-              ),
-              MapAnimationOptions(
-                duration: 4,
-              ),
-            );
-            circleAnnotation = annotation;
-          },
-        ),
-      );
-    });
+    if (isFirstLaunch) {
+      await prefs.setBool('isFirstLaunch', false);
+
+      PermissionStatus status = await Permission.location.request();
+      if (status.isGranted) {
+        print('Location permission granted');
+      } else if (status.isDenied) {
+        print('Location permission denied');
+      } else if (status.isPermanentlyDenied) {
+        await _showAppSettingsDialog();
+      }
+    }
   }
 
-  String getMapStyleUri(ThemeMode themeMode, BuildContext context) {
+  Future<void> _showAppSettingsDialog() async {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Permission Required"),
+        content: Text("Location permission is required for this feature. Please allow it in settings."),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text("Cancel"),
+          ),
+          TextButton(
+            onPressed: () async {
+              Navigator.of(context).pop();
+              await openAppSettings();
+            },
+            child: Text("Open Settings"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _addMarkersToMap(MapboxMap mapboxMap) async {
+    final ByteData bytes = await rootBundle.load('feature_modules/explore/assets/mensa_pin.png');
+    final Uint8List imageData = bytes.buffer.asUint8List();
+    final MbxImage mbxImage = MbxImage(
+      data: imageData,
+      width: LmuSizes.small.floor(),
+      height: LmuSizes.small.floor(),
+    );
+
+    await mapboxMap.style.addStyleImage(
+      "mensa_pin",
+      5,
+      mbxImage,
+      false,
+      [],
+      [],
+      null,
+    );
+  }
+
+  Future<void> _onMapCreated(MapboxMap mapboxMap) async {
+    this.mapboxMap = mapboxMap;
+
+    await _addMarkersToMap(mapboxMap);
+    pointAnnotationManager = await mapboxMap.annotations.createPointAnnotationManager();
+
+    var options = <PointAnnotationOptions>[];
+    for (final location in mensaLocations) {
+      options.add(
+        PointAnnotationOptions(
+          geometry: Point(
+            coordinates: Position(
+              location.longitude,
+              location.latitude,
+            ),
+          ),
+          iconImage: "mensa_pin",
+          iconSize: 1,
+        ),
+      );
+    }
+
+    await pointAnnotationManager?.createMulti(options);
+
+    pointAnnotationManager?.addOnPointAnnotationClickListener(
+      AnnotationClickListener(
+        onAnnotationClick: (annotation) {
+          mapboxMap.easeTo(
+            CameraOptions(
+              center: annotation.geometry,
+              zoom: 14.0,
+            ),
+            MapAnimationOptions(
+              duration: 4,
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  String _getMapStyleUri(ThemeMode themeMode, BuildContext context) {
     const String mapStyleLight = MapboxStyles.MAPBOX_STREETS;
     const String mapStyleDark = MapboxStyles.DARK;
 
@@ -99,6 +164,7 @@ class MapWithAnnotationsState extends State<MapWithAnnotations> {
   @override
   void initState() {
     super.initState();
+    _requestLocationPermission();
     mensaLocations = GetIt.I.get<MensaPublicApi>().testLocations;
   }
 
@@ -111,7 +177,7 @@ class MapWithAnnotationsState extends State<MapWithAnnotations> {
   Widget build(BuildContext context) {
     return Consumer<ThemeProvider>(
       builder: (context, themeProvider, child) {
-        String mapStyleUri = getMapStyleUri(themeProvider.themeMode, context);
+        String mapStyleUri = _getMapStyleUri(themeProvider.themeMode, context);
         WidgetsBinding.instance.addPostFrameCallback((_) {
           if (mapboxMap != null) {
             mapboxMap!.loadStyleURI(mapStyleUri);
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:lmu_app/lmu_app.dart';
-import 'package:lmu_app/registry/module_registry.dart';
+import 'lmu_app.dart';
+import 'registry/module_registry.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'registry/feature_modules.dart';
@@ -12,8 +13,10 @@ void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
-  final modulRegistry = ModuleRegistry(modules: modules);
-  await modulRegistry.init();
+  await dotenv.load(fileName: '.env');
+
+  final moduleRegistry = ModuleRegistry(modules: modules);
+  await moduleRegistry.init();
 
   runApp(
     LmuApp(),
