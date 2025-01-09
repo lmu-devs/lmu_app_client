c118d5c24723745eb206e3ef787cb982b80226a5
Timothy Summers <62808178+timothymaxwellsummers@users.noreply.github.com>
2025-01-09 18:46:43 +0100

    165 home sticky skeleton (#171)
    * feat: added sticky header
    
    * feat: added base for skeleton loading
    
    * feat: added wip disclaimer

--- feature_modules/home/lib/src/views/home_overview_view.dart
+++ feature_modules/home/lib/src/views/home_overview_view.dart
@@ -1,8 +1,11 @@
 import 'package:core/components.dart';
 import 'package:core/constants.dart';
+import 'package:core/localizations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_lucide/flutter_lucide.dart';
 import '../repository/api/models/home_model.dart';
 import 'home_links_view.dart';
+import 'package:flutter_lucide/flutter_lucide.dart';
 
 class HomeOverviewView extends StatelessWidget {
   const HomeOverviewView({
@@ -14,44 +17,59 @@ class HomeOverviewView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(children: [
-        const SizedBox(height: LmuSizes.size_16),
-        HomeLinksView(links: homeData.links),
-        Padding(
-          padding: const EdgeInsets.symmetric(
-              horizontal: LmuSizes.size_16, vertical: LmuSizes.size_16),
-          child: LmuContentTile(
-            content: [
-              LmuListItem.base(
-                title: "Studienbeitrag",
-                trailingTitleInTextVisuals: [
-                  LmuInTextVisual.text(
-                      title: homeData.submissionFee,
-                      color: Colors.deepOrangeAccent),
-                ],
-                mainContentAlignment: MainContentAlignment.center,
-              ),
-              LmuListItem.base(
-                title: "Vorlesungszeit",
-                trailingTitleInTextVisuals: [
-                  LmuInTextVisual.text(
-                      title: homeData.lectureTime, color: Colors.blue),
-                ],
-                mainContentAlignment: MainContentAlignment.center,
-              ),
-              LmuListItem.base(
-                title: "Vorlesungsfreiezeit",
-                trailingTitleInTextVisuals: [
-                  LmuInTextVisual.text(
-                      title: homeData.lectureFreeTime, color: Colors.blue),
-                ],
-                mainContentAlignment: MainContentAlignment.center,
-              ),
-            ],
-          ),
+    return Column(children: [
+      const SizedBox(height: LmuSizes.size_24),
+      HomeLinksView(links: homeData.links),
+      Padding(
+        padding: const EdgeInsets.symmetric(
+            horizontal: LmuSizes.size_16, vertical: LmuSizes.size_16),
+        child: LmuContentTile(
+          content: [
+            LmuListItem.base(
+              title: "Studienbeitrag",
+              trailingTitleInTextVisuals: [
+                LmuInTextVisual.text(
+                    title: homeData.submissionFee,
+                    color: Colors.deepOrangeAccent),
+              ],
+              mainContentAlignment: MainContentAlignment.center,
+            ),
+            LmuListItem.base(
+              title: "Vorlesungszeit",
+              trailingTitleInTextVisuals: [
+                LmuInTextVisual.text(
+                    title: homeData.lectureTime, color: Colors.blue),
+              ],
+              mainContentAlignment: MainContentAlignment.center,
+            ),
+            LmuListItem.base(
+              title: "Vorlesungsfreiezeit",
+              trailingTitleInTextVisuals: [
+                LmuInTextVisual.text(
+                    title: homeData.lectureFreeTime, color: Colors.blue),
+              ],
+              mainContentAlignment: MainContentAlignment.center,
+            ),
+          ],
         ),
-      ]),
-    );
+      ),
+      const SizedBox(height: LmuSizes.size_72),
+      Center(
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Icon(LucideIcons.construction, size: 16, color: Colors.grey),
+            const SizedBox(width: 8),
+            LmuText.body(
+              "This tab is work in progress",
+              color: Colors.grey,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(width: 8),
+            const Icon(LucideIcons.construction, size: 16, color: Colors.grey),
+          ],
+        ),
+      ),
+    ]);
   }
 }
