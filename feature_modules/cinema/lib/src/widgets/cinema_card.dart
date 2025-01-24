1c6a6d39556750c9c8eb6a08398f686e85043851
Lukas Plenk <lukasplenk@yahoo.de>
2025-01-24 23:44:24 +0100

    Refactored cinema and screening widgets

---
+++ feature_modules/cinema/lib/src/widgets/cinema_card.dart
@@ -0,0 +1,44 @@
+import 'package:core/components.dart';
+import 'package:core/constants.dart';
+import 'package:core/themes.dart';
+import 'package:flutter/material.dart';
+
+import '../pages/cinema_details_page.dart';
+import '../repository/api/api.dart';
+
+class CinemaCard extends StatelessWidget {
+  const CinemaCard({
+    Key? key,
+    required this.cinema,
+    required this.isLastItem,
+  }) : super(key: key);
+
+  final CinemaModel cinema;
+  final bool isLastItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        bottom: isLastItem ? LmuSizes.none : LmuSizes.size_12,
+      ),
+      padding: const EdgeInsets.all(LmuSizes.size_4),
+      decoration: BoxDecoration(
+        color: context.colors.neutralColors.backgroundColors.tile,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(LmuRadiusSizes.mediumLarge),
+        ),
+      ),
+      child: LmuListItem.base(
+        title: cinema.title,
+        titleInTextVisuals: [LmuInTextVisual.text(title: cinema.id)],
+        subtitle: 'Nächster Film • Morgen',
+        hasHorizontalPadding: true,
+        hasVerticalPadding: true,
+        onTap: () => Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => CinemaDetailsPage(cinema: cinema),
+        )),
+      ),
+    );
+  }
+}
--- feature_modules/cinema/lib/src/widgets/cinema_content_view.dart
+++ feature_modules/cinema/lib/src/widgets/cinema_content_view.dart
@@ -1,13 +1,12 @@
 import 'package:core/components.dart';
 import 'package:core/constants.dart';
 import 'package:core/localizations.dart';
-import 'package:core/themes.dart';
-import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 
-import '../pages/cinema_details_page.dart';
 import '../repository/api/api.dart';
 import '../repository/api/models/screening_model.dart';
+import 'cinema_card.dart';
+import 'screening_card.dart';
 
 class CinemaContentView extends StatelessWidget {
   const CinemaContentView({
@@ -30,130 +29,18 @@ class CinemaContentView extends StatelessWidget {
             LmuTileHeadline.base(title: context.locals.cinema.cinemasTitle),
             ...List.generate(cinemas.length, (index) {
               final cinema = cinemas[index];
-              return Container(
-                margin: EdgeInsets.only(
-                  bottom: index == cinemas.length - 1 ? LmuSizes.none : LmuSizes.size_12,
-                ),
-                padding: const EdgeInsets.all(LmuSizes.size_4),
-                decoration: BoxDecoration(
-                  color: context.colors.neutralColors.backgroundColors.tile,
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(LmuRadiusSizes.mediumLarge),
-                  ),
-                ),
-                child: LmuListItem.base(
-                  title: cinema.title,
-                  titleInTextVisuals: [LmuInTextVisual.text(title: cinema.id)],
-                  subtitle: 'Nächster Film • Morgen',
-                  hasHorizontalPadding: true,
-                  hasVerticalPadding: true,
-                  onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                    //CinemaDetailsRoute(cinema).go(context),
-                    builder: (context) => CinemaDetailsPage(cinema: cinema),
-                  )),
-                ),
+              return CinemaCard(
+                cinema: cinema,
+                isLastItem: index == cinemas.length - 1,
               );
             }),
             const SizedBox(height: LmuSizes.size_32),
             LmuTileHeadline.base(title: context.locals.cinema.moviesTitle),
             ...List.generate(screenings.length, (index) {
               final screening = screenings[index];
-              return GestureDetector(
-                onTap: () => print(screening.movie.title),
-                child: Container(
-                  margin: EdgeInsets.only(
-                    bottom: index == screenings.length - 1 ? LmuSizes.none : LmuSizes.size_12,
-                  ),
-                  decoration: BoxDecoration(
-                    color: context.colors.neutralColors.backgroundColors.tile,
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(LmuRadiusSizes.mediumLarge),
-                    ),
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: 165,
-                        width: 116,
-                        child: screening.movie.poster != null
-                            ? ClipRRect(
-                                borderRadius: const BorderRadius.only(
-                                  topLeft: Radius.circular(LmuRadiusSizes.mediumLarge),
-                                  bottomLeft: Radius.circular(LmuRadiusSizes.mediumLarge),
-                                ),
-                                child: FutureBuilder(
-                                  future: precacheImage(NetworkImage(screening.movie.poster!.url), context),
-                                  builder: (context, snapshot) {
-                                    if (snapshot.connectionState == ConnectionState.done) {
-                                      return Image.network(
-                                        screening.movie.poster!.url,
-                                        height: 165,
-                                        width: 116,
-                                        fit: BoxFit.cover,
-                                        semanticLabel: screening.movie.poster!.name,
-                                      );
-                                    } else {
-                                      return LmuSkeleton(
-                                        child: Container(
-                                          height: 165,
-                                          width: 116,
-                                          color: Colors.white,
-                                        ),
-                                      );
-                                    }
-                                  },
-                                ),
-                              )
-                            : Center(child: LmuText.caption(screening.movie.title)),
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(LmuSizes.size_16),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Column(
-                                mainAxisSize: MainAxisSize.min,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Flexible(
-                                    child: LmuText.body(
-                                      screening.movie.title,
-                                      maxLines: null,
-                                      customOverFlow: TextOverflow.visible,
-                                    ),
-                                  ),
-                                  const SizedBox(height: LmuSizes.size_4),
-                                  LmuText.bodySmall(
-                                    DateFormat("hh:mm • dd.MM.yyyy").format(DateTime.parse(screening.entryTime)),
-                                    color: context.colors.neutralColors.textColors.mediumColors.base,
-                                  ),
-                                  const SizedBox(height: LmuSizes.size_4),
-                                  LmuText.bodySmall(
-                                    'Sparche',
-                                    color: context.colors.neutralColors.textColors.mediumColors.base,
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  LmuInTextVisual.text(title: screening.cinema.id),
-                                  const SizedBox(width: LmuSizes.size_2),
-                                  //LmuInTextVisual.text(title: ''), // screening.movie.budget.toString()
-                                  const SizedBox(width: LmuSizes.size_2),
-                                  //LmuInTextVisual.text(title: ''), // screening.movie.ratings.first.normalizedRating.toString()
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+              return ScreeningCard(
+                screening: screening,
+                isLastItem: index == screenings.length - 1,
               );
             }),
             const SizedBox(height: LmuSizes.size_96),
