0f629de943d897c5cd19dccf586a17faa3483f3e
Lukas Plenk <lukasplenk@yahoo.de>
2025-01-26 23:31:48 +0100

    Added sorting to screenings history page

--- feature_modules/cinema/lib/src/pages/screenings_history_page.dart
+++ feature_modules/cinema/lib/src/pages/screenings_history_page.dart
@@ -1,12 +1,14 @@
 import 'package:core/components.dart';
 import 'package:core/constants.dart';
 import 'package:core/localizations.dart';
+import 'package:core/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_lucide/flutter_lucide.dart';
 
 import '../repository/api/models/screening_model.dart';
 import '../widgets/screening_card.dart';
 
-class ScreeningsHistoryPage extends StatelessWidget {
+class ScreeningsHistoryPage extends StatefulWidget {
   const ScreeningsHistoryPage({
     super.key,
     required this.screenings,
@@ -15,27 +17,182 @@ class ScreeningsHistoryPage extends StatelessWidget {
   final List<ScreeningModel> screenings;
 
   @override
+  State<ScreeningsHistoryPage> createState() => _ScreeningsHistoryPageState();
+}
+
+class _ScreeningsHistoryPageState extends State<ScreeningsHistoryPage> {
+  late ValueNotifier<SortOption> _sortOptionNotifier;
+  late ValueNotifier<List<ScreeningModel>> _sortedScreeningsNotifier;
+
+  @override
+  void initState() {
+    super.initState();
+    _sortOptionNotifier = ValueNotifier(SortOption.recentFirst);
+    _sortedScreeningsNotifier = ValueNotifier(
+      _sortOptionNotifier.value.sort(widget.screenings),
+    );
+  }
+
+  @override
+  void dispose() {
+    _sortOptionNotifier.dispose();
+    _sortedScreeningsNotifier.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final localizations = context.locals.cinema;
+
     return LmuMasterAppBar(
-      largeTitle: context.locals.cinema.pastMoviesTitle,
+      largeTitle: localizations.pastMoviesTitle,
       leadingAction: LeadingAction.back,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: LmuSizes.size_16),
         child: SingleChildScrollView(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ...List.generate(screenings.length, (index) {
-                final screening = screenings[index];
-                return ScreeningCard(
-                  screening: screening,
-                  isLastItem: index == screenings.length - 1,
-                );
-              }),
+              ValueListenableBuilder<SortOption>(
+                valueListenable: _sortOptionNotifier,
+                builder: (context, activeSortOption, _) {
+                  return LmuButton(
+                    title: activeSortOption.title(localizations),
+                    emphasis: ButtonEmphasis.secondary,
+                    trailingIcon: LucideIcons.chevron_down,
+                    onTap: () => _showSortOptionActionSheet(context),
+                  );
+                },
+              ),
+              const SizedBox(height: LmuSizes.size_16),
+              ValueListenableBuilder<List<ScreeningModel>>(
+                valueListenable: _sortedScreeningsNotifier,
+                builder: (context, sortedScreenings, _) {
+                  return AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 500),
+                    switchInCurve: Curves.easeInOut,
+                    switchOutCurve: Curves.easeInOut,
+                    reverseDuration: const Duration(milliseconds: 50),
+                    transitionBuilder: (child, animation) {
+                      return SlideTransition(
+                        position: Tween<Offset>(begin: const Offset(0, .7), end: Offset.zero).animate(animation),
+                        child: FadeTransition(opacity: animation, child: child),
+                      );
+                    },
+                    child: ListView.builder(
+                      key: ValueKey(sortedScreenings.map((screening) => screening.id).join()),
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      padding: EdgeInsets.zero,
+                      itemCount: sortedScreenings.length,
+                      itemBuilder: (context, index) {
+                        final screening = sortedScreenings[index];
+                        return ScreeningCard(
+                          screening: screening,
+                          isLastItem: index == sortedScreenings.length - 1,
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
               const SizedBox(height: LmuSizes.size_96),
             ],
           ),
         ),
       ),
     );
+  }
+
+  void _showSortOptionActionSheet(BuildContext context) {
+    LmuBottomSheet.show(
+      context,
+      content: _SortOptionActionSheetContent(
+        sortOptionNotifier: _sortOptionNotifier,
+        sortedScreeningsNotifier: _sortedScreeningsNotifier,
+        screenings: widget.screenings,
+      ),
+    );
+  }
+}
+
+class _SortOptionActionSheetContent extends StatelessWidget {
+  const _SortOptionActionSheetContent({
+    required this.sortOptionNotifier,
+    required this.sortedScreeningsNotifier,
+    required this.screenings,
+  });
+
+  final ValueNotifier<SortOption> sortOptionNotifier;
+  final ValueNotifier<List<ScreeningModel>> sortedScreeningsNotifier;
+  final List<ScreeningModel> screenings;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: SortOption.values.map((sortOption) {
+        final isActive = sortOption == sortOptionNotifier.value;
+
+        return LmuListItem.base(
+          title: sortOption.title(context.locals.cinema),
+          titleColor: isActive ? context.colors.brandColors.textColors.strongColors.base : null,
+          leadingArea: LmuIcon(
+            icon: sortOption.icon,
+            size: LmuIconSizes.medium,
+            color: isActive ? context.colors.brandColors.textColors.strongColors.base : null,
+          ),
+          onTap: () {
+            sortOptionNotifier.value = sortOption;
+            sortedScreeningsNotifier.value = sortOption.sort(screenings);
+            Navigator.of(context).pop();
+          },
+        );
+      }).toList(),
+    );
+  }
+}
+
+enum SortOption {
+  alphabetically,
+  recentFirst,
+  recentLast,
+}
+
+extension SortOptionExtension on SortOption {
+  IconData get icon {
+    switch (this) {
+      case SortOption.alphabetically:
+        return LucideIcons.a_large_small;
+      case SortOption.recentFirst:
+        return LucideIcons.arrow_down;
+      case SortOption.recentLast:
+        return LucideIcons.arrow_up;
+    }
+  }
+
+  String title(CinemaLocalizations localizations) {
+    switch (this) {
+      case SortOption.alphabetically:
+        return localizations.screeningsSortingAlphabetical;
+      case SortOption.recentFirst:
+        return localizations.screeningsSortingMostRecent;
+      case SortOption.recentLast:
+        return localizations.screeningsSortingLeastRecent;
+    }
+  }
+
+  List<ScreeningModel> sort(List<ScreeningModel> screenings) {
+    switch (this) {
+      case SortOption.alphabetically:
+        return List.from(screenings)
+          ..sort((a, b) => a.movie.title.compareTo(b.movie.title));
+      case SortOption.recentFirst:
+        return List.from(screenings)
+          ..sort((a, b) => DateTime.parse(a.entryTime).isAfter(DateTime.parse(b.entryTime)) ? -1 : 1);
+      case SortOption.recentLast:
+        return List.from(screenings)
+          ..sort((a, b) => DateTime.parse(b.entryTime).isAfter(DateTime.parse(a.entryTime)) ? -1 : 1);
+    }
   }
 }
