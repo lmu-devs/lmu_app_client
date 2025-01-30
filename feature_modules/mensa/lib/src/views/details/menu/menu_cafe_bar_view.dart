4df4512e86a0b5e6fe5441fdd3a0475fa464792c
Lukas Plenk <lukasplenk@yahoo.de>
2025-01-30 21:42:53 +0100

    Updated canteen texts

--- feature_modules/mensa/lib/src/views/details/menu/menu_cafe_bar_view.dart
+++ feature_modules/mensa/lib/src/views/details/menu/menu_cafe_bar_view.dart
@@ -1,5 +1,10 @@
+import 'package:core/components.dart';
 import 'package:core/constants.dart';
+import 'package:core/localizations.dart';
+import 'package:core/themes.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_lucide/flutter_lucide.dart';
 
 class MenuCafeBarView extends StatelessWidget {
   const MenuCafeBarView({super.key});
@@ -13,11 +18,29 @@ class MenuCafeBarView extends StatelessWidget {
       ),
       child: Column(
         children: [
-          Container(
-            width: double.infinity,
-            height: 25,
-            color: Colors.red,
-          )
+          Wrap(
+            spacing: LmuSizes.size_8,
+            children: [
+              Icon(
+                LucideIcons.coffee,
+                size: LmuIconSizes.large,
+                color: context.colors.neutralColors.textColors.mediumColors.base,
+              ),
+              Icon(
+                LucideIcons.croissant,
+                size: LmuIconSizes.large,
+                color: context.colors.neutralColors.textColors.mediumColors.base,
+              )
+            ],
+          ),
+          const SizedBox(height: LmuSizes.size_16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: LmuSizes.size_16),
+            child: LmuText.body(
+              context.locals.canteen.cafeBarInfo,
+              textAlign: TextAlign.center,
+            ),
+          ),
         ],
       ),
     );
