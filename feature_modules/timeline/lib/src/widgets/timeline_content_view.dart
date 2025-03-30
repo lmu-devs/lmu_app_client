8062aa34b0fa1d5b40d4ac33415e56a0254e586f
Paul W. <65418896+BitterSchoki@users.noreply.github.com>
2025-03-30 20:08:31 +0200

    (feat): implement sticky semester/moths for timeline (#271)
    * (feat): implement sticky semester/moths for timeline
    
    * (chore): remove old timeline page
    
    ---------
    
    Co-authored-by: Paul <paul.walter@partner.bmw.de>

--- feature_modules/timeline/lib/src/widgets/timeline_content_view.dart
+++ feature_modules/timeline/lib/src/widgets/timeline_content_view.dart
@@ -1,197 +1,158 @@
-import 'package:collection/collection.dart';
 import 'package:core/components.dart';
 import 'package:core/constants.dart';
+import 'package:core/localizations.dart';
 import 'package:core/themes.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
-import '../repository/api/models/timeline_data.dart';
-import '../repository/api/models/timeline_event.dart';
-import '../repository/api/models/timeline_semester.dart';
+import '../repository/api/models/models.dart';
 
 class TimelineContentView extends StatelessWidget {
-  const TimelineContentView({super.key, required this.timelineData});
+  const TimelineContentView({super.key, required this.data});
 
-  final TimelineData timelineData;
+  final TimelineData data;
 
   @override
   Widget build(BuildContext context) {
-    final semesters = timelineData.semesters;
-    final events = timelineData.events;
-    final Map<TimelineSemester, List<TimelineEvent>> semesterEvents = {};
+    final colors = context.colors;
 
-    for (final semester in semesters) {
-      semesterEvents[semester] = events.where((event) {
-        return event.timeframe.start.isBefore(semester.timeframe.end) &&
-            event.timeframe.end.isAfter(semester.timeframe.start);
-      }).toList();
-    }
+    final grouped = groupEventsBySemesterAndMonth(context, data);
 
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          ...semesterEvents.keys.mapIndexed(
-            (index, semester) {
-              final events = semesterEvents[semester] ?? [];
-              final currentEvents = events.where((event) {
-                return event.timeframe.start.isBefore(DateTime.now()) && event.timeframe.end.isAfter(DateTime.now());
-              }).toList();
-              final oldEvents = events.where((event) {
-                return event.timeframe.end.isBefore(DateTime.now());
-              }).toList();
-              final upcomingEvents = events.where((event) {
-                return event.timeframe.start.isAfter(DateTime.now());
-              }).toList();
+    return CustomScrollView(
+      slivers: [
+        const SliverToBoxAdapter(child: SizedBox(height: LmuSizes.size_8)),
+        MultiSliver(
+          children: grouped.entries.map((semesterEntry) {
+            final semesterLabel = semesterEntry.key;
+            final monthMap = semesterEntry.value;
 
-              return Column(
-                children: [
-                  SizedBox(height: index == 0 ? LmuSizes.size_16 : LmuSizes.size_32),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: LmuSizes.size_16),
-                    child: LmuTileHeadline.base(
-                      title: semester.type.localizedName(),
-                      trailingTitle: DateFormat("dd.MM.yyyy").format(semester.timeframe.start),
-                    ),
-                  ),
-                  if (oldEvents.isNotEmpty)
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: LmuSizes.size_16),
-                      child: LmuContentTile(
-                        contentList: oldEvents.mapIndexed(
-                          (index, event) {
-                            final eventStart = event.timeframe.start;
-                            final eventEnd = event.timeframe.end;
+            return SliverStickyHeader(
+              header: Container(
+                color: colors.neutralColors.backgroundColors.base,
+                padding: const EdgeInsets.only(left: LmuSizes.size_16, right: LmuSizes.size_16, top: LmuSizes.size_8),
+                child: LmuText.h3(semesterLabel),
+              ),
+              sliver: MultiSliver(
+                children: monthMap.entries.map((monthEntry) {
+                  final monthLabel = monthEntry.key;
+                  final events = monthEntry.value;
 
-                            final isInSameYear = eventStart.year == eventEnd.year;
+                  events.sort((a, b) => a.timeframe.start.compareTo(b.timeframe.start));
 
-                            final isInPreviousYear = eventStart.year < eventEnd.year;
-                            final formattedStart = (isInSameYear && !isInPreviousYear)
-                                ? DateFormat("dd.MM.").format(eventStart)
-                                : DateFormat("dd.MM.yyyy").format(eventStart);
-
-                            final formattedEnd = (isInSameYear && !isInPreviousYear)
-                                ? DateFormat("dd.MM.").format(eventEnd)
-                                : DateFormat("dd.MM.yyyy").format(eventEnd);
-
-                            final timeSinceEnd = DateTime.now().difference(event.timeframe.end).inDays;
-                            return LmuListItem.base(
-                              title: event.title,
-                              mainContentAlignment: MainContentAlignment.top,
-                              subtitle: "vor $timeSinceEnd ${timeSinceEnd > 1 ? "Tagen" : "Tag"}",
-                              trailingTitle: formattedStart,
-                              trailingSubtitle: (formattedStart != formattedEnd) ? "bis $formattedEnd" : null,
-                              hasDivider: index != oldEvents.length - 1,
-                            );
-                          },
-                        ).toList(),
-                      ),
-                    ),
-                  if (currentEvents.isNotEmpty)
-                    Padding(
-                      padding: EdgeInsets.only(
-                        left: 6.0,
-                        right: 16.0,
-                        top: (oldEvents.isNotEmpty) ? LmuSizes.size_8 : 0,
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          LayoutBuilder(
-                            builder: (context, constraints) {
-                              return Container(
-                                width: LmuSizes.size_4,
-                                height: 48,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(4),
-                                  color: context.colors.brandColors.backgroundColors.strongColors.base,
+                  return SliverStickyHeader.builder(
+                    builder: (context, state) => Container(
+                      padding: const EdgeInsets.only(
+                          left: LmuSizes.size_16, right: LmuSizes.size_16, bottom: LmuSizes.size_8),
+                      decoration: BoxDecoration(
+                        color: colors.neutralColors.backgroundColors.base,
+                        border: state.isPinned
+                            ? Border(
+                                bottom: BorderSide(
+                                  color: colors.neutralColors.borderColors.seperatorLight,
+                                  width: 1 - state.scrollPercentage,
                                 ),
-                              );
-                            },
-                          ),
-                          const SizedBox(width: 6),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width - 32,
-                            child: LmuContentTile(
-                              contentList: currentEvents.mapIndexed(
-                                (index, event) {
-                                  final eventStart = event.timeframe.start;
-                                  final eventEnd = event.timeframe.end;
-
-                                  final isInSameYear = eventStart.year == eventEnd.year;
-
-                                  final formattedStart = isInSameYear
-                                      ? DateFormat("dd.MM.").format(eventStart)
-                                      : DateFormat("dd.MM.yyyy").format(eventStart);
-
-                                  final formattedEnd = DateFormat("dd.MM.yyyy").format(eventEnd);
-                                  final daysUntilEnd = eventEnd.difference(DateTime.now()).inDays;
-
-                                  return LmuListItem.base(
-                                    title: event.title,
-                                    mainContentAlignment: MainContentAlignment.top,
-                                    subtitle: "noch $daysUntilEnd Tage",
-                                    trailingTitle: formattedStart,
-                                    trailingSubtitle: (formattedStart != formattedEnd) ? "bis $formattedEnd" : null,
-                                    hasDivider: index != currentEvents.length - 1,
-                                  );
-                                },
-                              ).toList(),
+                              )
+                            : null,
+                      ),
+                      child: LmuText.body(
+                        monthLabel,
+                        weight: FontWeight.w600,
+                        color: colors.neutralColors.textColors.mediumColors.base,
+                      ),
+                    ),
+                    sliver: SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(
+                              left: LmuSizes.size_16,
+                              right: LmuSizes.size_16,
+                              bottom: LmuSizes.size_32,
                             ),
-                          ),
-                        ],
+                            child: LmuContentTile(
+                              contentList: events.map((event) {
+                                return LmuListItem.base(
+                                  leadingArea: LmuText.body(
+                                    "${event.timeframe.start.day.toString().padLeft(2, '0')}.",
+                                    weight: FontWeight.w600,
+                                  ),
+                                  title: event.title,
+                                );
+                              }).toList(),
+                            ),
+                          );
+                        },
+                        childCount: 1,
                       ),
                     ),
-                  if (upcomingEvents.isNotEmpty)
-                    Padding(
-                      padding: EdgeInsets.only(
-                        left: LmuSizes.size_16,
-                        right: LmuSizes.size_16,
-                        top: (oldEvents.isNotEmpty || currentEvents.isNotEmpty) ? LmuSizes.size_8 : 0,
-                      ),
-                      child: LmuContentTile(
-                        contentList: upcomingEvents.mapIndexed(
-                          (index, event) {
-                            final eventStart = event.timeframe.start;
-                            final eventEnd = event.timeframe.end;
-
-                            final isInSameYear = eventStart.year == eventEnd.year;
-
-                            final formattedStart = isInSameYear
-                                ? DateFormat("dd.MM.").format(eventStart)
-                                : DateFormat("dd.MM.yyyy").format(eventStart);
-
-                            final formattedEnd = isInSameYear
-                                ? DateFormat("dd.MM.").format(eventEnd)
-                                : DateFormat("dd.MM.yyyy").format(eventEnd);
-                            final timeUntilStart = eventStart.difference(DateTime.now()).inDays;
-                            return LmuListItem.base(
-                              title: event.title,
-                              mainContentAlignment: MainContentAlignment.top,
-                              subtitle: "in $timeUntilStart ${timeUntilStart > 1 ? "Tagen" : "Tag"}",
-                              trailingTitle: formattedStart,
-                              trailingSubtitle: (formattedStart != formattedEnd) ? "bis $formattedEnd" : null,
-                              hasDivider: index != upcomingEvents.length - 1,
-                            );
-                          },
-                        ).toList(),
-                      ),
-                    ),
-                ],
-              );
-            },
-          ),
-          const SizedBox(height: LmuSizes.size_96),
-        ],
-      ),
+                  );
+                }).toList(),
+              ),
+            );
+          }).toList(),
+        ),
+        const SliverToBoxAdapter(child: SizedBox(height: LmuSizes.size_96)),
+      ],
     );
   }
 }
 
-extension on SemesterType {
-  String localizedName() {
-    return switch (this) {
-      SemesterType.summer => "Sommersemester",
-      SemesterType.winter => "Wintersemester",
-    };
+Map<String, Map<String, List<TimelineEvent>>> groupEventsBySemesterAndMonth(
+  BuildContext context,
+  TimelineData data,
+) {
+  final timelineLocals = context.locals.timeline;
+  final result = <String, Map<String, List<TimelineEvent>>>{};
+
+  for (final semester in data.semesters) {
+    final semesterLabel = semester.formatSemesterLabel(timelineLocals);
+    result.putIfAbsent(semesterLabel, () => {});
+  }
+
+  for (final event in data.events) {
+    final eventStart = event.timeframe.start;
+
+    final semester = data.semesters.firstWhere(
+      (s) => eventStart.isAfter(s.timeframe.start) && eventStart.isBefore(s.timeframe.end),
+      orElse: () => throw Exception("No matching semester found for event"),
+    );
+
+    final semesterLabel = semester.formatSemesterLabel(timelineLocals);
+    final monthLabel = eventStart.localizedMonth(timelineLocals);
+
+    result[semesterLabel]!.putIfAbsent(monthLabel, () => []);
+    result[semesterLabel]![monthLabel]!.add(event);
+  }
+
+  return result;
+}
+
+extension on TimelineSemester {
+  String formatSemesterLabel(TimelineLocatizations locals) {
+    final year = timeframe.start.year;
+    final nextYear = timeframe.start.month >= 10 ? year + 1 : year;
+    final semesterStr = type == SemesterType.winter ? "${locals.winter} $year/$nextYear" : "${locals.summer} $nextYear";
+    return semesterStr;
+  }
+}
+
+extension on DateTime {
+  String localizedMonth(TimelineLocatizations locals) {
+    final monthNames = [
+      locals.january,
+      locals.february,
+      locals.march,
+      locals.april,
+      locals.may,
+      locals.june,
+      locals.july,
+      locals.august,
+      locals.september,
+      locals.october,
+      locals.november,
+      locals.december
+    ];
+    return monthNames[month - 1];
   }
 }
