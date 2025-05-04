4fa3de2f9d46fb088c6a767bf6999de3ff376daf
Paul <paul.walter@partner.bmw.de>
2025-05-04 18:35:06 +0200

    (feat): update toast implementation to get instace from context

--- core/lib/src/components/toasts/base_toast.dart
+++ core/lib/src/components/toasts/base_toast.dart
@@ -4,22 +4,14 @@ import 'package:core/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-enum ToastType {
-  base,
-  success,
-  error,
-  warning,
-}
+enum ToastType { base, success, error, warning }
 
 class LmuToast {
-  static void removeAll({
-    required BuildContext context,
-  }) {
-    final fToast = FToast();
-    fToast.init(context);
+  final FToast _fToast;
 
-    fToast.removeCustomToast();
-  }
+  LmuToast._(BuildContext context) : _fToast = FToast()..init(context);
+
+  static LmuToast of(BuildContext context) => LmuToast._(context);
 
   static void show({
     required BuildContext context,
@@ -28,114 +20,141 @@ class LmuToast {
     VoidCallback? onActionPressed,
     ToastType type = ToastType.base,
     Duration? duration,
-  }) {
-    final fToast = FToast();
-    fToast.init(context);
+  }) =>
+      LmuToast._(context)._showToast(
+        message: message,
+        actionText: actionText,
+        onActionPressed: onActionPressed,
+        type: type,
+        duration: duration,
+      );
 
-    final toast = SlideTransition(
-      position: TweenSequence<Offset>([
-        TweenSequenceItem(
-          tween: Tween<Offset>(
-            begin: const Offset(0.0, 1.0),
-            end: const Offset(0.0, 0.0),
-          ).chain(CurveTween(curve: LmuAnimations.quick)),
-          weight: 1.0,
-        ),
-      ]).animate(
-        AnimationController(
-          vsync: Navigator.of(context),
-          duration: const Duration(milliseconds: 1000),
-        )..forward(),
-      ),
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: LmuSizes.size_16, vertical: LmuSizes.size_8),
-        decoration: BoxDecoration(
-          color: context.colors.neutralColors.backgroundColors.base,
-          borderRadius: BorderRadius.circular(50),
-          border: Border.all(
-            color: context.colors.neutralColors.borderColors.seperatorLight,
-            width: 1,
-          ),
-        ),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              width: LmuSizes.size_8,
-              height: LmuSizes.size_8,
-              decoration: BoxDecoration(
-                color: type.color(context.colors),
-                shape: BoxShape.circle,
-              ),
-            ),
-            const SizedBox(width: LmuSizes.size_12),
-            Flexible(
-              child: LmuText.bodySmall(
-                message,
-                weight: FontWeight.w600,
-                customOverFlow: TextOverflow.ellipsis,
-                maxLines: 3,
-              ),
-            ),
-            if (actionText != null && onActionPressed != null) ...[
-              Row(
-                children: [
-                  const SizedBox(width: LmuSizes.size_8),
-                  Container(
-                    width: 1,
-                    height: 20,
-                    color: context.colors.neutralColors.backgroundColors.mediumColors.base,
-                  ),
-                  const SizedBox(width: LmuSizes.size_8),
-                  LmuButton(
-                    title: actionText,
-                    onTap: () {
-                      onActionPressed();
-                      fToast.removeCustomToast();
-                    },
-                    size: ButtonSize.medium,
-                    emphasis: ButtonEmphasis.link,
-                  )
-                ],
-              ),
-            ],
-          ],
-        ),
-      ),
+  void showToast({
+    required String message,
+    String? actionText,
+    VoidCallback? onActionPressed,
+    ToastType type = ToastType.base,
+    Duration? duration,
+  }) =>
+      _showToast(
+        message: message,
+        actionText: actionText,
+        onActionPressed: onActionPressed,
+        type: type,
+        duration: duration,
+      );
+
+  void _showToast({
+    required String message,
+    String? actionText,
+    VoidCallback? onActionPressed,
+    ToastType type = ToastType.base,
+    Duration? duration,
+  }) {
+    final toast = _buildToast(
+      message: message,
+      actionText: actionText,
+      onActionPressed: onActionPressed,
+      type: type,
     );
 
-    fToast.showToast(
+    _fToast.showToast(
       child: toast,
       gravity: ToastGravity.BOTTOM,
       toastDuration: duration ??
           _calculateDuration(
-            messages: [message, if (actionText != null && onActionPressed != null) actionText],
+            messages: [message, if (actionText != null) actionText],
             hasAction: onActionPressed != null,
           ),
       fadeDuration: const Duration(milliseconds: 200),
     );
   }
 
-  /// Calculates the duration of the toast based on the number of words in the message
+  Widget _buildToast({
+    required String message,
+    String? actionText,
+    VoidCallback? onActionPressed,
+    required ToastType type,
+  }) {
+    return Builder(
+      builder: (context) {
+        return SafeArea(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: LmuSizes.size_16, vertical: LmuSizes.size_8),
+            decoration: BoxDecoration(
+              color: context.colors.neutralColors.backgroundColors.base,
+              borderRadius: BorderRadius.circular(50),
+              border: Border.all(
+                color: context.colors.neutralColors.borderColors.seperatorLight,
+                width: 1,
+              ),
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  width: LmuSizes.size_8,
+                  height: LmuSizes.size_8,
+                  decoration: BoxDecoration(
+                    color: type.color(context.colors),
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                const SizedBox(width: LmuSizes.size_12),
+                Flexible(
+                  child: LmuText.bodySmall(
+                    message,
+                    weight: FontWeight.w600,
+                    customOverFlow: TextOverflow.ellipsis,
+                    maxLines: 3,
+                  ),
+                ),
+                if (actionText != null && onActionPressed != null) ...[
+                  Row(
+                    children: [
+                      const SizedBox(width: LmuSizes.size_8),
+                      Container(
+                        width: 1,
+                        height: 20,
+                        color: context.colors.neutralColors.backgroundColors.mediumColors.base,
+                      ),
+                      const SizedBox(width: LmuSizes.size_8),
+                      LmuButton(
+                        title: actionText,
+                        onTap: () {
+                          onActionPressed();
+                          _fToast.removeCustomToast();
+                        },
+                        size: ButtonSize.medium,
+                        emphasis: ButtonEmphasis.link,
+                      )
+                    ],
+                  ),
+                ],
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  static void removeAll({required BuildContext context}) {
+    final fToast = FToast()..init(context);
+    fToast.removeCustomToast();
+  }
+
   static Duration _calculateDuration({
     required List<String> messages,
     bool hasAction = false,
   }) {
     int wordCount = messages
-        .where((msg) => msg.trim().isNotEmpty) // Filter out empty/whitespace-only strings
-        .map((msg) => msg.trim().split(RegExp(r'\s+')).length) // Split into words
-        .fold(0, (sum, count) => sum + count); // Sum up word counts
+        .where((msg) => msg.trim().isNotEmpty)
+        .map((msg) => msg.trim().split(RegExp(r'\s+')).length)
+        .fold(0, (sum, count) => sum + count);
 
-    int durationMs = 0;
-    if (hasAction) {
-      // Add 1 second for the action button
-      durationMs += 1000;
-    }
-
-    // Base duration: 1 second + 250ms per word
-    durationMs += 1000 + (wordCount * 250);
-
-    // Clamp between 1,6 seconds and 5 seconds
+    int durationMs = 1000 + (wordCount * 250);
+    if (hasAction) durationMs += 1000;
     return Duration(milliseconds: durationMs.clamp(1600, 4000));
   }
 }
