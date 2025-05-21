6d9aa715f83f45eb6e90f1bb892378d2cb591f53
SirAntony <rockenstein.anton@gmail.com>
2025-05-21 20:04:57 +0200

    card integration

--- feature_modules/home/lib/src/pages/id_card_page.dart
+++ feature_modules/home/lib/src/pages/id_card_page.dart
@@ -1,7 +1,6 @@
 import 'package:core/components.dart';
 import 'package:core/constants.dart';
 import 'package:flutter/material.dart';
-import '../../../../../core/lib/src/components/student_id/themes/themes.dart';
 
 class IdCardPage extends StatelessWidget {
   const IdCardPage({super.key});
@@ -13,9 +12,9 @@ class IdCardPage extends StatelessWidget {
         largeTitle: "Student ID",
         leadingAction: LeadingAction.back,
       ),
-      body: const Center(
+      body: Center(
         child: Padding(
-          padding: EdgeInsets.all(LmuSizes.size_16),
+          padding: const EdgeInsets.all(LmuSizes.size_16),
           child: HolographicCard(
             name: "John Doe",
             email: "john.doe@example.com",
@@ -27,13 +26,16 @@ class IdCardPage extends StatelessWidget {
             height: 220,
             borderRadius: 20,
             borderWidth: 2,
-            // cardColor: LmuCardThemes.greenTheme.cardColor,
-            // textColor: LmuCardThemes.greenTheme.textColor,
-            // secondaryTextColor: LmuCardThemes.greenTheme.secondaryTextColor,
-            // logoColor: LmuCardThemes.greenTheme.logoColor,
-            // hologramColor: theme.hologramColor,
-            // cardColor: Colors.white,
-            //borderCardColor: LmuCardThemes.greenTheme.cardColor,
+            cardColor: LmuCardThemes.greenTheme.cardColor,
+            textColor: LmuCardThemes.greenTheme.textColor,
+            secondaryTextColor: LmuCardThemes.greenTheme.secondaryTextColor,
+            logoColor: LmuCardThemes.greenTheme.logoColor,
+            hologramColor: LmuCardThemes.greenTheme.hologramColor,
+            hologramAsset:
+                'core/lib/src/components/student_id/holograms/LMU-Sigel.svg',
+            hologramAsset2:
+                'core/lib/src/components/student_id/holograms/LMUcard.svg',
+            logoAsset: 'core/lib/src/components/student_id/logos/LMU-Logo.svg',
           ),
         ),
       ),
