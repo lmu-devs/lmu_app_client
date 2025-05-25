6322ff23971f9cfa377d33027ce4e71666feb16c
Paul <paul.walter@partner.bmw.de>
2025-05-25 21:46:42 +0200

    (feat): reintegrate app update screen

--- core/lib/src/utils/app_update_navigator.dart
+++ core/lib/src/utils/app_update_navigator.dart
@@ -3,5 +3,5 @@ import 'package:go_router/go_router.dart';
 class AppUpdateNavigator {
   static late GoRouter router;
 
-  static void popAllAndNavigate() => router.go('/app_update');
+  static void popAllAndNavigate() => router.pushReplacement('/app_update');
 }
