74b997a819a416abb51cced1998773a1895b6bb5
Lukas Plenk <lukasplenk@yahoo.de>
2025-05-25 16:50:56 +0200

    feat: Improved visual distinction of wishlist items

--- feature_modules/wishlist/lib/src/pages/wishlist_page.dart
+++ feature_modules/wishlist/lib/src/pages/wishlist_page.dart
@@ -63,7 +63,7 @@ class _WishlistPageState extends State<WishlistPage> {
                 color: context.colors.neutralColors.textColors.mediumColors.base,
               ),
             ),
-            const SizedBox(height: LmuSizes.size_24),
+            const SizedBox(height: LmuSizes.size_32),
             LmuButtonRow(
               buttons: [
                 LmuButton(
@@ -126,10 +126,10 @@ class _WishlistPageState extends State<WishlistPage> {
                       ),
                     ],
                   ),
-                  const SizedBox(height: LmuSizes.size_24),
+                  const SizedBox(height: LmuSizes.size_32),
                   LmuTileHeadline.base(title: context.locals.wishlist.wishlistEntriesTitle),
                   const WishlistEntryList(),
-                  const SizedBox(height: LmuSizes.size_24),
+                  const SizedBox(height: LmuSizes.size_16),
                   LmuContentTile(
                     contentList: [
                       LmuListItem.base(
