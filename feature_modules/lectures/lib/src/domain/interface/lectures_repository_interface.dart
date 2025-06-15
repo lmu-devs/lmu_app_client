d6257574a7ab36860d0b17c220a529f7d6065135
Paul <paul.walter@partner.bmw.de>
2025-06-15 15:47:48 +0200

    core: update repository docu

--- feature_modules/lectures/lib/src/domain/interface/lectures_repository_interface.dart
+++ feature_modules/lectures/lib/src/domain/interface/lectures_repository_interface.dart
@@ -1,14 +1,14 @@
+import '../exception/lectures_generic_exception.dart';
 import '../model/lectures.dart';
 
 abstract class LecturesRepositoryInterface {
-  
   /// Fetches the latest Lectures data from the remote source.
-  /// 
-  /// Throws a [DomainException] on failure.
+  ///
+  /// Throws a [LecturesGenericException] on failure.
   Future<Lectures> getLectures();
-  
+
   /// Retrieves cached Lectures data, if available.
-  /// 
+  ///
   /// Returns `null` if no cached data exists.
   Future<Lectures?> getCachedLectures();
 
