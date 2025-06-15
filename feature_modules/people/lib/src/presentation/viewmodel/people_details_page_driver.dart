634f3711781dadb28acf7b306357f17866be6ba4
leoniedbm <you@example.com>
2025-06-15 13:19:31 +0200

    changes for widget

--- feature_modules/people/lib/src/presentation/viewmodel/people_details_page_driver.dart
+++ feature_modules/people/lib/src/presentation/viewmodel/people_details_page_driver.dart
@@ -1,12 +1,8 @@
-import 'package:core/components.dart';
-import 'package:core/localizations.dart';
-import 'package:core_routes/people.dart';
 import 'package:get_it/get_it.dart';
 import 'package:widget_driver/widget_driver.dart';
 
 import '../../application/state/people_state.dart';
 import '../../domain/model/people.dart';
-import '../../domain/model/people_category.dart';
 import '../view/people_details_page.dart';
 
 part 'people_details_page_driver.g.dart';
@@ -27,13 +23,19 @@ class PeopleDetailsPageDriver extends WidgetDriver {
   ))
   late final People person;
 
+  late String _id;
+
   @override
   void didInitDriver() {
     super.didInitDriver();
-    final id = (widget as PeopleDetailsPage).id;
-    person = _peopleStateService.state.value.peopleCategories
-        .expand((cat) => cat.peoples)
-        .firstWhere((p) => p.id == id);
+    person =
+        _peopleStateService.state.value.peopleCategories.expand((cat) => cat.peoples).firstWhere((p) => p.id == _id);
+  }
+
+  @override
+  void didUpdateBuildContext(BuildContext context) {
+    super.didUpdateBuildContext(context);
+    _id = (context.widget as PeopleDetailsPage).id;
   }
 
   String get name => person.name;
