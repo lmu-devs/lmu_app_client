59eee70627876a02e8e2f90ffafc25a8d789f721
leoniedbm <you@example.com>
2025-07-08 22:49:13 +0200

    added attributes to people dto part 2 (+ onTap Navigation)

--- feature_modules/people/lib/src/presentation/viewmodel/people_details_page_driver.dart
+++ feature_modules/people/lib/src/presentation/viewmodel/people_details_page_driver.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/widgets.dart';
 import 'package:get_it/get_it.dart';
 import 'package:widget_driver/widget_driver.dart';
-import 'package:core_routes/people.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../application/usecase/get_people_usecase.dart';
 import '../../domain/model/people.dart';
@@ -14,15 +15,44 @@ class PeopleDetailsPageDriver extends WidgetDriver implements _$DriverProvidedPr
   }) : _personId = personId;
 
   late int _personId;
+
+  @override
   int get personId => _personId;
 
   final _usecase = GetIt.I.get<GetPeopleUsecase>();
 
   People? get person => _usecase.data.where((p) => p.id == personId).firstOrNull;
+  bool get isLoading => _usecase.loadState != PeopleLoadState.success;
 
-  String get largeTitle {
-    final person = _usecase.data.firstWhere((p) => p.id == personId);
-    return person.name;
+  String get faculty => person?.faculty ?? '';
+  String get role => person?.role ?? '';
+  String get email => person?.email ?? '';
+  String get phone => person?.phone ?? '';
+  String get website => person?.website ?? '';
+  String get room => person?.room ?? '';
+  String get consultation => person?.consultation ?? '';
+
+  Future<void> onEmailTap() async {
+    print('onEmailTap');
+  }
+
+  Future<void> onPhoneTap() async {
+    print('onPhoneTap');
+  }
+
+  Future<void> onWebsiteTap() async {
+    final webUrl = Uri.parse(website);
+    if (await canLaunchUrl(webUrl)) {
+      await launchUrl(webUrl, mode: LaunchMode.externalApplication);
+    }
+  }
+
+  Future<void> onRoomTap() async {
+    print('onRoomTap');
+  }
+
+  Future<void> onConsultationTap() async {
+    print('onConsultationTap');
   }
 
   @override
@@ -34,15 +64,16 @@ class PeopleDetailsPageDriver extends WidgetDriver implements _$DriverProvidedPr
     }
   }
 
-  void _onStateChanged() {
-    notifyWidget();
-  }
-
   @override
   void didUpdateProvidedProperties({
     required int newPersonId,
   }) {
     _personId = newPersonId;
+    notifyWidget();
+  }
+
+  void _onStateChanged() {
+    notifyWidget();
   }
 
   @override
