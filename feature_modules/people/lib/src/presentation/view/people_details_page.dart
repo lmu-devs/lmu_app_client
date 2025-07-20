ab37f6d4ce98655aa6578587dd93892739cb7f61
leoniedbm <you@example.com>
2025-07-20 11:55:24 +0200

    Added design adjustements as in Figma, SIngleChildScrollView for render flex -> too long subtitle

--- feature_modules/people/lib/src/presentation/view/people_details_page.dart
+++ feature_modules/people/lib/src/presentation/view/people_details_page.dart
@@ -1,6 +1,9 @@
 import 'package:core/components.dart';
 import 'package:core/constants.dart';
+import 'package:core/localizations.dart';
+import 'package:core/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_lucide/flutter_lucide.dart';
 import 'package:widget_driver/widget_driver.dart';
 
 import '../viewmodel/people_details_page_driver.dart';
@@ -52,66 +55,101 @@ class PeopleDetailsPage extends DrivableWidget<PeopleDetailsPageDriver> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(
-          LmuSizes.size_16,
-          LmuSizes.size_32,
-          LmuSizes.size_16,
-          LmuSizes.size_16,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            LmuTileHeadline.base(title: driver.facultyAndRoleText),
-            const SizedBox(height: LmuSizes.size_2),
-            LmuContentTile(
-              contentList: [
-                LmuListItem.base(
-                  title: driver.faculty,
-                ),
-                LmuListItem.base(
-                  title: driver.role,
-                ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(
+            LmuSizes.size_16,
+            LmuSizes.size_2, 
+            LmuSizes.size_16,
+            LmuSizes.size_16,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (driver.facultyAndRole.isNotEmpty) ...[
+                LmuTileHeadline.base(title: driver.facultyAndRole),
+                const SizedBox(height: LmuSizes.size_2),
               ],
-            ),
-            const SizedBox(height: LmuSizes.size_32),
-            LmuTileHeadline.base(title: driver.contactText),
-            const SizedBox(height: LmuSizes.size_2),
-            LmuContentTile(
-              contentList: [
-                LmuListItem.base(
-                  title: driver.emailText,
-                  subtitle: driver.email,
-                  trailingArea: const Icon(Icons.mail_outline),
-                  onTap: () => driver.onEmailTap(context),
-                ),
-                LmuListItem.base(
-                  title: driver.phoneText,
-                  subtitle: driver.phone,
-                  trailingArea: const Icon(Icons.phone_outlined),
-                  onTap: () => driver.onPhoneTap(context),
-                ),
-                LmuListItem.base(
-                  title: driver.websiteText,
-                  subtitle: driver.website,
-                  trailingArea: const Icon(Icons.open_in_new),
-                  onTap: () => driver.onWebsiteTap(context),
-                ),
-                LmuListItem.base(
-                  title: driver.roomText,
-                  subtitle: driver.room,
-                  //trailingArea: const Icon(Icons.map_outlined),
-                  onTap: () => driver.onRoomTap(),
-                ),
-                LmuListItem.base(
-                  title: driver.consultationHoursText,
-                  subtitle: driver.consultation,
-                  //trailingArea: const Icon(Icons.schedule_outlined),
-                  onTap: () => driver.onConsultationTap(),
-                ),
-              ],
-            ),
-          ],
+              
+              const SizedBox(height: LmuSizes.size_32), 
+              LmuTileHeadline.base(title: driver.contactText),
+              const SizedBox(height: LmuSizes.size_2),
+              LmuContentTile(
+                contentList: [
+                  if (driver.email.isNotEmpty)
+                    LmuListItem.base(
+                      title: driver.email,
+                      subtitle: driver.emailText,
+                      leadingArea: LmuIconButton(
+                        icon: LucideIcons.mail,
+                        onPressed: () => driver.onEmailTap(context),
+                      ),
+                      trailingArea: LmuIconButton(
+                        icon: LucideIcons.copy,
+                        onPressed: () => CopyToClipboardUtil.copyToClipboard(
+                          context: context,
+                          copiedText: driver.email,
+                          message: driver.copiedToClipboardText,
+                        ),
+                      ),
+                    ),
+                  if (driver.phone.isNotEmpty)
+                    LmuListItem.base(
+                      title: driver.phone,
+                      subtitle: driver.phoneText,
+                      leadingArea: LmuIconButton(
+                        icon: LucideIcons.phone,
+                        onPressed: () => driver.onPhoneTap(context),
+                      ),
+                      trailingArea: LmuIconButton(
+                        icon: LucideIcons.copy,
+                        onPressed: () => CopyToClipboardUtil.copyToClipboard(
+                          context: context,
+                          copiedText: driver.phone,
+                          message: driver.copiedPhoneText,
+                        ),
+                      ),
+                    ),
+                  if (driver.website.isNotEmpty)
+                    LmuListItem.base(
+                      title: driver.website,
+                      subtitle: driver.websiteText,
+                      leadingArea: LmuIconButton(
+                        icon: LucideIcons.globe,
+                        onPressed: () => driver.onWebsiteTap(context),
+                      ),
+                      trailingArea: LmuIconButton(
+                        icon: LucideIcons.copy,
+                        onPressed: () => CopyToClipboardUtil.copyToClipboard(
+                          context: context,
+                          copiedText: driver.website,
+                          message: driver.copiedToClipboardText,
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+              const SizedBox(height: LmuSizes.size_16),
+              LmuContentTile(
+                contentList: [
+                  if (driver.room.isNotEmpty)
+                    LmuListItem.base(
+                      title: driver.roomText,
+                      subtitle: driver.room,
+                      trailingArea: LmuIconButton(
+                        icon: LucideIcons.map_pin,
+                        onPressed: () => driver.onRoomTap(),
+                      ),
+                    ),
+                  if (driver.consultation.isNotEmpty)
+                    LmuListItem.base(
+                      title: driver.consultationHoursText,
+                      subtitle: driver.consultation,
+                    ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
